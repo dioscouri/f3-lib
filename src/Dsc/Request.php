1dcdf49906f8dd91cf6cfb21cb3f04bed122586c
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2013-12-14 13:34:10 -0500

    added \Dsc\Request for basic HMVC requests, fixes #16.

---
+++ src/Dsc/Request.php
@@ -0,0 +1,32 @@
+<?php 
+namespace Dsc;
+
+class Request extends Object
+{
+    /**
+     * Attempts to load the specified controller and execute the requested method with the provided arguments
+     * 
+     * @param string $controller_method
+     * @param array $arguments
+     * @param array $options
+     * 
+     * @return string 
+     */
+    public static function internal( $controller_method, $arguments=array(), $options=array() ) 
+    {
+        $f3 = \Base::instance();
+        $f3->set('HMVC', true);
+        
+        $hooks = !empty($options['hooks']) ? $options['hooks'] : null;
+        
+        try {
+            $return = $f3->call( $controller_method, $arguments, $hooks );
+        } catch (\Exception $e) {
+            $return = $e->getMessage();
+        }
+        
+        $f3->set('HMVC', false);
+        
+        return (string) $return;
+    }
+}
