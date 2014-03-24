479e744167ef6578da99aa656ac46ed416a6d90e
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-03-24 13:47:10 -0400

    code formatting

--- src/Dsc/Bootstrap.php
+++ src/Dsc/Bootstrap.php
@@ -1,147 +1,168 @@
-<?php 
+<?php
 namespace Dsc;
 
 /**
  * This is base bootstrap class for every f3 application enabling us to hook up some services before
  * the real bootstrapping occurs
- * 
  */
-abstract class Bootstrap{
+abstract class Bootstrap
+{
+    protected $namespace = '';
+    
+    /**
+     * This needs to be set in bootstrap.php of every app
+     */
+    protected $dir = '';
 
-	protected $namespace = '';
-	
-	/**
-	 * This needs to be set in bootstrap.php of every app
-	 */
-	protected $dir = '';
-	
-	/**
-	 * Triggers a command for a specific part of application
-	 * 
-	 * @param $name Name of command
-	 * @param $app Name of part of application
-	 */
-	public function command( $name, $app ){
-		$app = ucwords($app);
-		if( method_exists( $this, $name.$app ) ){
-			$func = $name.$app;
-			$this->$func();
-		} else if( method_exists( $this, $name ) ){
-			$this->$name( $app );
-		}
-	}
-	
-	protected function run($app){
-		// handle other types of application, if no specific function defined
-	}
-	
-	protected function runAdmin(){
-		$listener = "\\".$this->namespace."\\Listener";
-		if( class_exists( $listener ) ){
-			// register event listener
-			\Dsc\System::instance()->getDispatcher()->addListener($listener::instance());
-		}
-		$listener = "\\".$this->namespace."\\Admin\Listener";
-		if( class_exists( $listener ) ){
-		    // register event listener
-		    \Dsc\System::instance()->getDispatcher()->addListener($listener::instance());
-		}		
-		$this->_runBase('Admin');
-	}
+    /**
+     * Triggers a command for a specific part of application
+     *
+     * @param $name Name
+     *            of command
+     * @param $app Name
+     *            of part of application
+     */
+    public function command( $name, $app )
+    {
+        $app = ucwords( $app );
+        if (method_exists( $this, $name . $app ))
+        {
+            $func = $name . $app;
+            $this->$func();
+        }
+        else if (method_exists( $this, $name ))
+        {
+            $this->$name( $app );
+        }
+    }
 
-	protected function runSite(){
-	    $listener = "\\".$this->namespace."\\Listener";
-	    if( class_exists( $listener ) ){
-	        // register event listener
-	        \Dsc\System::instance()->getDispatcher()->addListener($listener::instance());
-	    }
-	    $listener = "\\".$this->namespace."\\Site\Listener";
-	    if( class_exists( $listener ) ){
-	        // register event listener
-	        \Dsc\System::instance()->getDispatcher()->addListener($listener::instance());
-	    }
-		$this->_runBase('Site');
-	}
-	
-	/**
-	 * This part is common for all running all parts of application (both Admin and Site)
-	 * 
-	 * @param $app Name of the part of application
-	 */
-	private function _runBase( $app ){
-		$f3 = \Base::instance();
-		$router = "\\".$this->namespace."\\Routes";
-		if( !class_exists($router)){
-			$router = "\\".$this->namespace."\\".$app."\\Routes";
-			if( !class_exists($router)){
-				$router = '';
-			}
-		}
-		if( strlen( $router ) ) {
-			// register all the routes
-			\Dsc\System::instance()->get('router')->mount( new $router, $this->namespace );
-		}
+    protected function run( $app )
+    {
+        // handle other types of application, if no specific function defined
+    }
 
-		// register the modules path, if you can
-		$modules_path = $this->dir."/src/".$this->namespace."/Modules/";
-		if( !file_exists( $modules_path ) ){
-			// let's try more specific route
-			$modules_path = $this->dir."/src/".$this->namespace.'/'.$app. "/Modules/";
-			if( !file_exists( $modules_path ) ){ // not even here? maybe more luck next time
-				$modules_path = '';
-			}
-		}
-		
-		if( strlen( $modules_path ) ){
-			\Modules\Factory::registerPath( $modules_path );			
-		}
-		
-		// append this app's UI folder to the path
-		// new way
-		
-		if( file_exists( $this->dir . '/src/'.$this->namespace.'/'.$app.'/Views/' ) ){
-			\Dsc\System::instance()->get('theme')->registerViewPath(
-					$this->dir . '/src/'.$this->namespace.'/'.$app.'/Views/',
-					$this->namespace.'/'.$app.'/Views'
-			);
-			// old way
-			$ui = $f3->get('UI');
-			$ui .= ";" .$this->dir . "/src/".$this->namespace.'/'.$app."/Views/";
-			$f3->set('UI', $ui);
-		} else {
-			if( file_exists( $this->dir . '/src/'.$this->namespace.'/Views/' ) ){
-				\Dsc\System::instance()->get('theme')->registerViewPath(
-						$this->dir . '/src/'.$this->namespace.'/Views/',
-						$this->namespace.'/Views'
-				);
-				// old way
-				$ui = $f3->get('UI');
-				$ui .= ";" .$this->dir . "/src/".$this->namespace."/Views/";
-				$f3->set('UI', $ui);
-					
-			}
-		}
-		
-	}
-	
-	
-	protected function pre($app){
-		// handle other types of application, if no specific function defined
-	}
-	
-	protected function preAdmin(){
-	}
+    protected function runAdmin()
+    {
+        $listener = "\\" . $this->namespace . "\\Listener";
+        if (class_exists( $listener ))
+        {
+            // register event listener
+            \Dsc\System::instance()->getDispatcher()->addListener( $listener::instance() );
+        }
+        $listener = "\\" . $this->namespace . "\\Admin\Listener";
+        if (class_exists( $listener ))
+        {
+            // register event listener
+            \Dsc\System::instance()->getDispatcher()->addListener( $listener::instance() );
+        }
+        $this->_runBase( 'Admin' );
+    }
 
-	protected function preSite(){
-	}
-	
-	protected function post($app){
-		// handle other types of application, if no specific function defined
-	}
-	
-	protected function postAdmin(){
-	}
-	
-	protected function postSite(){
-	}
+    protected function runSite()
+    {
+        $listener = "\\" . $this->namespace . "\\Listener";
+        if (class_exists( $listener ))
+        {
+            // register event listener
+            \Dsc\System::instance()->getDispatcher()->addListener( $listener::instance() );
+        }
+        $listener = "\\" . $this->namespace . "\\Site\Listener";
+        if (class_exists( $listener ))
+        {
+            // register event listener
+            \Dsc\System::instance()->getDispatcher()->addListener( $listener::instance() );
+        }
+        $this->_runBase( 'Site' );
+    }
+
+    /**
+     * This part is common for all running all parts of application (both Admin and Site)
+     *
+     * @param $app Name
+     *            of the part of application
+     */
+    private function _runBase( $app )
+    {
+        $f3 = \Base::instance();
+        $router = "\\" . $this->namespace . "\\Routes";
+        if (! class_exists( $router ))
+        {
+            $router = "\\" . $this->namespace . "\\" . $app . "\\Routes";
+            if (! class_exists( $router ))
+            {
+                $router = '';
+            }
+        }
+        if (strlen( $router ))
+        {
+            // register all the routes
+            \Dsc\System::instance()->get( 'router' )->mount( new $router(), $this->namespace );
+        }
+        
+        // register the modules path, if you can
+        $modules_path = $this->dir . "/src/" . $this->namespace . "/Modules/";
+        if (! file_exists( $modules_path ))
+        {
+            // let's try more specific route
+            $modules_path = $this->dir . "/src/" . $this->namespace . '/' . $app . "/Modules/";
+            if (! file_exists( $modules_path ))
+            { // not even here? maybe more luck next time
+                $modules_path = '';
+            }
+        }
+        
+        if (strlen( $modules_path ))
+        {
+            \Modules\Factory::registerPath( $modules_path );
+        }
+        
+        // append this app's UI folder to the path
+        // new way
+        
+        if (file_exists( $this->dir . '/src/' . $this->namespace . '/' . $app . '/Views/' ))
+        {
+            \Dsc\System::instance()->get( 'theme' )->registerViewPath( $this->dir . '/src/' . $this->namespace . '/' . $app . '/Views/', $this->namespace . '/' . $app . '/Views' );
+            // old way
+            $ui = $f3->get( 'UI' );
+            $ui .= ";" . $this->dir . "/src/" . $this->namespace . '/' . $app . "/Views/";
+            $f3->set( 'UI', $ui );
+        }
+        else
+        {
+            if (file_exists( $this->dir . '/src/' . $this->namespace . '/Views/' ))
+            {
+                \Dsc\System::instance()->get( 'theme' )->registerViewPath( $this->dir . '/src/' . $this->namespace . '/Views/', $this->namespace . '/Views' );
+                // old way
+                $ui = $f3->get( 'UI' );
+                $ui .= ";" . $this->dir . "/src/" . $this->namespace . "/Views/";
+                $f3->set( 'UI', $ui );
+            }
+        }
+    }
+
+    protected function pre( $app )
+    {
+        // handle other types of application, if no specific function defined
+    }
+
+    protected function preAdmin()
+    {
+    }
+
+    protected function preSite()
+    {
+    }
+
+    protected function post( $app )
+    {
+        // handle other types of application, if no specific function defined
+    }
+
+    protected function postAdmin()
+    {
+    }
+
+    protected function postSite()
+    {
+    }
 }
