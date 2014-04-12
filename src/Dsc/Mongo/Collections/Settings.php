9c6ec58cce9c97cc2e5a12e9fdad826cba4e9d97
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-04-12 09:25:17 -0400

    change to the Settings Collection

--- src/Dsc/Mongo/Collections/Settings.php
+++ src/Dsc/Mongo/Collections/Settings.php
@@ -1,7 +1,7 @@
 <?php 
 namespace Dsc\Mongo\Collections;
 
-class Settings extends \Dsc\Mongo\Collections\Nodes 
+class Settings extends \Dsc\Mongo\Collection 
 {
     protected $__collection_name = 'common.settings';
     protected $__type = 'common.settings';
@@ -14,5 +14,26 @@ class Settings extends \Dsc\Mongo\Collections\Nodes
         $item = new static;
         $item->load(array('type' => $item->type()));
         return $item;
+    }
+    
+    /**
+     * Gets the type
+     */
+    public function type()
+    {
+        return $this->__type;
+    }
+
+    /**
+     * 
+     */
+    protected function beforeValidate()
+    {
+        if (empty($this->type))
+        {
+            $this->type = $this->__type;
+        }
+    
+        return parent::beforeValidate();
     }
 }
