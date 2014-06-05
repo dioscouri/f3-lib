797e3c55338b73af1f25da73818ebd457f9b8c3c
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-06-04 21:11:33 -0400

    added a \Models\Publishable trait

---
+++ src/Dsc/Traits/Models/Publishable.php
@@ -0,0 +1,104 @@
+<?php
+namespace Dsc\Traits\Models;
+
+trait Publishable
+{
+
+    /**
+     * Checks if the model is published as of today
+     *
+     * @param string $check_status            
+     * @param string $status_value            
+     * @return boolean
+     */
+    public function published($check_status = true, $status_value = 'published')
+    {
+        $return = false;
+        
+        if ($check_status && $this->{'publication.status'} != $status_value)
+        {
+            $return = false;
+        }
+        
+        if ((empty($this->{'publication.start.time'}) || $this->{'publication.start.time'} <= time()) && (empty($this->{'publication.end.time'}) || $this->{'publication.end.time'} >= time()))
+        {
+            if (!$check_status)
+            {
+                $return = true;
+            }
+            
+            elseif ($check_status && $this->{'publication.status'} == $status_value)
+            {
+                $return = true;
+            }
+            
+            elseif ($check_status && $this->{'publication.status'} != $status_value)
+            {
+                $return = false;
+            }
+        }
+        
+        return $return;
+    }
+
+    /**
+     * Changes the publication.status
+     *
+     * @param string $status_value            
+     */
+    public function publish($status_value = 'published')
+    {
+        return $this->update(array(
+            'publication.status' => $status_value
+        ), array(
+            'overwrite' => false
+        ));
+    }
+
+    /**
+     * Changes the publication.status
+     *
+     * @param string $status_value            
+     */
+    public function unpublish($status_value = 'unpublished')
+    {
+        return $this->update(array(
+            'publication.status' => $status_value
+        ), array(
+            'overwrite' => false
+        ));
+    }
+
+    protected function publishableBeforeSave()
+    {
+        if (!empty($this->{'publication.start_date'}))
+        {
+            $string = $this->{'publication.start_date'};
+            if (!empty($this->{'publication.start_time'}))
+            {
+                $string .= ' ' . $this->{'publication.start_time'};
+            }
+            $this->{'publication.start'} = \Dsc\Mongo\Metastamp::getDate(trim($string));
+        }
+        else
+        {
+            $this->{'publication.start'} = \Dsc\Mongo\Metastamp::getDate('now');
+        }
+        
+        if (empty($this->{'publication.end_date'}))
+        {
+            unset($this->{'publication.end'});
+        }
+        elseif (!empty($this->{'publication.end_date'}))
+        {
+            $string = $this->{'publication.end_date'};
+            if (!empty($this->{'publication.end_time'}))
+            {
+                $string .= ' ' . $this->{'publication.end_time'};
+            }
+            $this->{'publication.end'} = \Dsc\Mongo\Metastamp::getDate(trim($string));
+        }
+        
+        return $this;
+    }
+}
