29a9cf87708ef64ab520dbf005a80713f5e20a5c
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-06-06 13:25:46 -0400

    adding date-range filters for created and last-modified

--- src/Dsc/Mongo/Collections/Nodes.php
+++ src/Dsc/Mongo/Collections/Nodes.php
@@ -47,8 +47,114 @@ class Nodes extends \Dsc\Mongo\Collection
             $this->setCondition('metadata.creator.id', new \MongoId( (string) $filter_creator_id ) );
         }
         
-        // TODO Add date-range filters for created & last_modified
+        $filter_created_after = $this->getState('filter.created_after');
+        if (strlen($filter_created_after))
+        {
+            $filter_created_after = strtotime($filter_created_after);
+            
+            // add $and conditions to the query stack
+            if (!$and = $this->getCondition('$and'))
+            {
+                $and = array();
+            }
         
+            $and[] = array(
+                '$or' => array(
+                    array(
+                        'metadata.created.time' => null
+                    ),
+                    array(
+                        'metadata.created.time' => array(
+                            '$lte' => $filter_created_after
+                        )
+                    )
+                )
+            );
+        
+            $this->setCondition('$and', $and);
+        }
+        
+        $filter_created_before = $this->getState('filter.created_before');
+        if (strlen($filter_created_before))
+        {
+            $filter_created_before = strtotime($filter_created_before);
+        
+            // add $and conditions to the query stack
+            if (!$and = $this->getCondition('$and'))
+            {
+                $and = array();
+            }
+        
+            $and[] = array(
+                '$or' => array(
+                    array(
+                        'metadata.created.time' => null
+                    ),
+                    array(
+                        'metadata.created.time' => array(
+                            '$gt' => $filter_created_before
+                        )
+                    )
+                )
+            );
+        
+            $this->setCondition('$and', $and);
+        }    
+
+        $filter_last_modified_after = $this->getState('filter.last_modified_after');
+        if (strlen($filter_last_modified_after))
+        {
+            $filter_last_modified_after = strtotime($filter_last_modified_after);
+        
+            // add $and conditions to the query stack
+            if (!$and = $this->getCondition('$and'))
+            {
+                $and = array();
+            }
+        
+            $and[] = array(
+                '$or' => array(
+                    array(
+                        'metadata.last_modified.time' => null
+                    ),
+                    array(
+                        'metadata.last_modified.time' => array(
+                            '$lte' => $filter_last_modified_after
+                        )
+                    )
+                )
+            );
+        
+            $this->setCondition('$and', $and);
+        }
+        
+        $filter_last_modified_before = $this->getState('filter.last_modified_before');
+        if (strlen($filter_last_modified_before))
+        {
+            $filter_last_modified_before = strtotime($filter_last_modified_before);
+        
+            // add $and conditions to the query stack
+            if (!$and = $this->getCondition('$and'))
+            {
+                $and = array();
+            }
+        
+            $and[] = array(
+                '$or' => array(
+                    array(
+                        'metadata.last_modified.time' => null
+                    ),
+                    array(
+                        'metadata.last_modified.time' => array(
+                            '$gt' => $filter_last_modified_before
+                        )
+                    )
+                )
+            );
+        
+            $this->setCondition('$and', $and);
+        }
+                
         return $this;
     }
     
