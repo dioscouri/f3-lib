b13875df19c2f60b1456fd5955e5938378958e21
Lukas Polak <lpolak@dioscouri.com>
2014-06-14 07:41:10 -0400

    Small tweaking to bootstrapping apps

--- src/Dsc/Apps.php
+++ src/Dsc/Apps.php
@@ -43,7 +43,8 @@ class Apps extends Singleton
         {
             foreach ($folders as $folder)
             {
-                if (file_exists($path . $folder . '/bootstrap.php'))
+		        $app = null;
+            	if (file_exists($path . $folder . '/bootstrap.php'))
                 {
                     require_once $path . $folder . '/bootstrap.php';
                     if (!empty($app))
@@ -56,12 +57,12 @@ class Apps extends Singleton
         
         // then do the custom apps
         $path = $f3->get('PATH_ROOT') . 'apps/';
-        $app = null;
         if ($folders = \Joomla\Filesystem\Folder::folders($path))
         {
             foreach ($folders as $folder)
             {
-                if (file_exists($path . $folder . '/bootstrap.php'))
+		        $app = null;
+            	if (file_exists($path . $folder . '/bootstrap.php'))
                 {
                     require_once $path . $folder . '/bootstrap.php';
                     if (!empty($app))
@@ -81,7 +82,8 @@ class Apps extends Singleton
                 
                 foreach ($folders as $folder)
                 {
-                    
+                	$app = null;
+                	 
                     if (file_exists($additional_path . $folder . '/bootstrap.php'))
                     {
                         require_once $additional_path . $folder . '/bootstrap.php';
