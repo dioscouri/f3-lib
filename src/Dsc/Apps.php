1716ed257a3a47080d386281d7ea8ae91dc4d078
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-06-15 14:21:10 -0400

    further cleanup in dsc\apps, refs #49

--- src/Dsc/Apps.php
+++ src/Dsc/Apps.php
@@ -18,17 +18,16 @@ class Apps extends Singleton
         //\FB::warn(round(memory_get_usage(TRUE)/1e3,1) . ' KB');
                 
         $bootstraps = array(); // array of all app bootstrap classes
+        $paths = array_merge( array(), array( $this->app->get('PATH_ROOT') . 'vendor/dioscouri/', $this->app->get('PATH_ROOT') . 'apps/' ), $additional_paths );
         
         if (strpos($route, '/asset/') === 0) 
         {
             $app_name = 'f3-assets';
         }
         
+        // bootstrap a single app
         if (!empty($app_name))
         {
-            // TODO Use this array for NORMAL bootstrapping too
-            $paths = array_merge( array(), array( $this->app->get('PATH_ROOT') . 'vendor/dioscouri/', $this->app->get('PATH_ROOT') . 'apps/' ), $additional_paths );
-            
             $app = null;
             foreach ($paths as $path) 
             {
@@ -46,74 +45,31 @@ class Apps extends Singleton
         }
         
         // bootstrap all apps
-        // loop through each child folder (only 1st level) of the /apps folder
-        // if a bootstrap.php file exists, require it once
-        $f3 = \Base::instance();
         if (!defined('JPATH_ROOT'))
         {
-            define('JPATH_ROOT', $f3->get('PATH_ROOT'));
-        }
-                         
-        // do the original apps first
-        $path = $f3->get('PATH_ROOT') . 'vendor/dioscouri/';
-        if ($folders = \Joomla\Filesystem\Folder::folders($path))
-        {
-            foreach ($folders as $folder)
-            {
-                $app = null;
-                if (file_exists($path . $folder . '/bootstrap.php'))
-                {
-                    require_once $path . $folder . '/bootstrap.php';
-                    if (!empty($app) && is_a($app, '\Dsc\Bootstrap'))
-                    {
-                        $bootstraps[] = $app;
-                    }
-                }
-            }
+            define('JPATH_ROOT', $this->app->get('PATH_ROOT'));
         }
         
-        // then do the custom apps
-        $path = $f3->get('PATH_ROOT') . 'apps/';
-        if ($folders = \Joomla\Filesystem\Folder::folders($path))
+        foreach ($paths as $path)
         {
-            foreach ($folders as $folder)
+            if ($folders = \Joomla\Filesystem\Folder::folders($path))
             {
-                $app = null;
-                if (file_exists($path . $folder . '/bootstrap.php'))
-                {
-                    require_once $path . $folder . '/bootstrap.php';
-                    if (!empty($app) && is_a($app, '\Dsc\Bootstrap'))
-                    {
-                        $bootstraps[] = $app;
-                    }
-                }
-            }
-        }
-        
-        // then do any additional paths
-        foreach ($additional_paths as $additional_path)
-        {
-            if ($folders = \Joomla\Filesystem\Folder::folders($additional_path))
-            {
-                
                 foreach ($folders as $folder)
                 {
-                    $app = null;                    
-                    if (file_exists($additional_path . $folder . '/bootstrap.php'))
+                    $app = null;
+                    if (file_exists($path . $folder . '/bootstrap.php'))
                     {
-                        require_once $additional_path . $folder . '/bootstrap.php';
+                        require_once $path . $folder . '/bootstrap.php';
                         if (!empty($app) && is_a($app, '\Dsc\Bootstrap'))
                         {
                             $bootstraps[] = $app;
                         }
                     }
                 }
-            }
+            }            
         }
         
-        $this->load($bootstraps);
-        
-        return $this;
+        return $this->load($bootstraps);
     }
 
     /**
@@ -155,30 +111,19 @@ class Apps extends Singleton
                 
         if (!empty($bootstraps))
         {
-            foreach ($bootstraps as $key=>$bootstrap)
-            {     
-                if (empty($bootstrap) || !is_a($bootstrap, '\Dsc\Bootstrap')) 
-                {
-                	unset($bootstraps[$key]);
-                }
-            }
-            
             $global_app_name = $this->app->get('APP_NAME');
             foreach ($bootstraps as $bootstrap)
             {
-                //\FB::log('APPS-PRE: ', $bootstrap->name() );
                 $bootstrap->command('pre', $global_app_name);
             }
         
             foreach ($bootstraps as $bootstrap)
             {
-                //\FB::log('APPS-RUN: ', $bootstrap->name() );
                 $bootstrap->command('run', $global_app_name);
             }
         
             foreach ($bootstraps as $bootstrap)
             {
-                //\FB::log('APPS-POST: ', $bootstrap->name() );
                 $bootstrap->command('post', $global_app_name);
             }
         }
