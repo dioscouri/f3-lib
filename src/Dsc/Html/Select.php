229559fa2838eda42cd91e7306d307fdbbc5fcec
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-07-04 00:16:06 -0400

    formatting change only

--- src/Dsc/Html/Select.php
+++ src/Dsc/Html/Select.php
@@ -1,49 +1,54 @@
-<?php 
+<?php
 namespace Dsc\Html;
 
 class Select extends \Dsc\Singleton
-{	
-	/*
-	$option = array(
-    array('text' => display_text, 'value' => 'value', 'data' => array( 'key'=>value  ) )
-	);
-
-	*/
-    public static function options($options, $selected) {
-    	$html = '';
-    	if( empty( $options ) ){
-    		return '';
-    	}
-    	
-    	if(is_array($options[0])) {
-    		foreach ($options as  $option) {
-    			$html .= '<option value="';
-    			$html .= $option['value'];	
-    			$html .= '"';
-    			if($option['value'] == $selected) {
-    			$html .= ' selected="selected" ';	
-    			}
-    			if(!empty( $option['data'] ) &&  is_array($option['data'])) {
-    				foreach ($option['data'] as $key => $value) {
-    				$html .= ' data-'.$key.'="'.$value.'" ';	
-    				}
-    			}
-    			$html .= '>'.$option['text'].'</option>';
-    		}
-
-    	} else {
-    		foreach ($options as  $value) {
-    			$html .= '<option value="';
-    			$html .= $value;	
-    			$html .= '"';
-    			if($value == $selected) {
-    			$html .= ' selected="selected" ';	
-    			}
-    			$html .= '>'.ucwords($value).'</option>';
-    		}
-    	}
-    	return $html;
-
+{
+    /*
+     * $option = array( array('text' => display_text, 'value' => 'value', 'data' => array( 'key'=>value ) ) );
+     */
+    public static function options($options, $selected)
+    {
+        $html = '';
+        if (empty($options))
+        {
+            return '';
+        }
+        
+        if (is_array($options[0]))
+        {
+            foreach ($options as $option)
+            {
+                $html .= '<option value="';
+                $html .= $option['value'];
+                $html .= '"';
+                if ($option['value'] == $selected)
+                {
+                    $html .= ' selected="selected" ';
+                }
+                if (!empty($option['data']) && is_array($option['data']))
+                {
+                    foreach ($option['data'] as $key => $value)
+                    {
+                        $html .= ' data-' . $key . '="' . $value . '" ';
+                    }
+                }
+                $html .= '>' . $option['text'] . '</option>';
+            }
+        }
+        else
+        {
+            foreach ($options as $value)
+            {
+                $html .= '<option value="';
+                $html .= $value;
+                $html .= '"';
+                if ($value == $selected)
+                {
+                    $html .= ' selected="selected" ';
+                }
+                $html .= '>' . ucwords($value) . '</option>';
+            }
+        }
+        return $html;
     }
-
 }
