25ea877488f188076a1a28b5804bd76279c04fc4
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-07-08 00:04:02 -0400

    cleanup in cli app

--- src/Dsc/Cli/process_queue.php
+++ src/Dsc/Cli/process_queue.php
@@ -10,11 +10,6 @@ $app->set('APP_NAME', 'cli');
 
 require $app->get('PATH_ROOT') . 'config/config.php';
 
-// for CLI apps:
-$app->set('LOGS', $app->get('PATH_ROOT') . 'logs/');
-$app->set('TEMP', $app->get('PATH_ROOT') . 'tmp/');
-$app->set('db.jig.dir', $app->get('PATH_ROOT') . 'jig/');
-
 // bootstap each mini-app
 \Dsc\Apps::instance()->bootstrap();
 
