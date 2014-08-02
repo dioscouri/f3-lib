fcdae0adc8c69d8c764ce1e40cfbf970463b6e4a
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-08-02 10:06:28 -0400

    added actorTrack to global activities model, refs dioscouri/f3-activity#14

--- src/Dsc/Activities.php
+++ src/Dsc/Activities.php
@@ -42,4 +42,27 @@ class Activities extends Singleton
     
         return null;
     }
+    
+    public static function trackActor($email, $action, $properties=array())
+    {
+        if (class_exists('\Activity\Models\Actions'))
+        {
+            \Activity\Models\Actions::trackActor($email, $action, $properties);
+        }
+        
+        if (class_exists('\Admin\Models\Settings') && class_exists('\KM'))
+        {
+            $settings = \Admin\Models\Settings::fetch();
+            if ($settings->enabledIntegration('kissmetrics') && $settings->{'integration.kissmetrics.key'})
+            {
+                \KM::init( $settings->{'integration.kissmetrics.key'} );
+        
+                \KM::identify($email);
+        
+                \KM::record($action, $properties);
+            }
+        }        
+        
+        return null;
+    }    
 }
