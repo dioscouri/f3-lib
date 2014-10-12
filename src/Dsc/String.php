11a9d5c77b7505e41762d7afa3c338cc1850cd58
Chris French <chris@ammonitenetworks.com>
2014-10-12 09:14:33 -0600

    adding format phone method to string class

--- src/Dsc/String.php
+++ src/Dsc/String.php
@@ -26,4 +26,22 @@ class String
     {
         return array_map('trim', preg_split('/[,;|]/',$str,0,PREG_SPLIT_NO_EMPTY));
     }
+    
+    /**
+     *	Format phone  string
+     *
+     *	@return string
+     *	@param $str string
+     **/
+    public static function format_phone($phone)
+    {
+    	$phone = preg_replace("/[^0-9]/", "", $phone);
+    
+    	if(strlen($phone) == 7)
+    		return preg_replace("/([0-9]{3})([0-9]{4})/", "$1-$2", $phone);
+    	elseif(strlen($phone) == 10)
+    	return preg_replace("/([0-9]{3})([0-9]{3})([0-9]{4})/", "($1) $2-$3", $phone);
+    	else
+    		return $phone;
+    }
 }
