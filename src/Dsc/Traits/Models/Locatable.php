d7805ecd6d9757ac449910048ef53c89740805b7
Building Berms Server <chris@ammonitenetworks.com>
2014-11-16 16:57:03 +0000

    adding support for searching with a polygon

--- src/Dsc/Traits/Models/Locatable.php
+++ src/Dsc/Traits/Models/Locatable.php
@@ -19,18 +19,50 @@ trait Locatable
     	{
     		$this->setCondition('loc', array('$near' => array($filter_location_near[0], $filter_location_near[1])));
     	}
+    	
+    	$filter_location_within_poly = $this->getState('filter.location_within_poly');
+
+    	//MUST include array of arrays with long, lat;
+    	if (count($filter_location_within_poly) > 2)
+    	{
+    		
+    		/*EXAMPLE: 
+	    	 * $cords = array(
+	   		 *	array('51.12335082548444', '-114.19052124023438'),
+	   		 *	array('51.11904092252057', '-114.05593872070312'),
+	   		 *	array('51.02325750523972', '-114.02435302734375'),
+	   		 *	array('51.01634653617311', '-114.1644287109375'),
+	   		 *	);
+	       	 *	$item = $model->setState('filter.location_within_poly', $cords)->getItem();
+			 *      // found find an address with this cords
+			 *      "loc" : {
+			 *       "lon" : 51.08282186160978,
+			 *       "lat" : -114.10400390625
+			 *   },
+      		 * 
+    		 * 
+    		 */
+    		
+    		//CREATE POLY, and type set values
+    		$poly = array();
+    		$range = range('a', 'z');
+    		for ($i = 0; $i < count($filter_location_within_poly); $i++) {
+    			$poly[$range[$i]] = array('x' => (float) $filter_location_within_poly[$i][0], 'y'=>(float) $filter_location_within_poly[$i][1]);
+    		}
+    		
+    		$this->setCondition('loc', array('$within' => array('$polygon'=> $poly)));
+    	}
     
     	return $this;
     }
     
     
-    public function setLocation(array $location)
+    public function setLocation($lon, $lat)
     {
      	//TODO ENFORCE lon,lat
     	//ensure floats etc
     	
-    	
-        $this->set('loc',array((float) $location[0], (float) $location[1]));
+        $this->set('loc',array('lon' => (float) $lon, 'lat' => (float) $lat));
        
         $this;
     }
